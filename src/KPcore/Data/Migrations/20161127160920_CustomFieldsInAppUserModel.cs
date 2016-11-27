fe89ee16bcef97146086d2a93a4b891a2eacd3c7
Gabriel <gabriel.mackiewicz@gmail.com>
2016-11-27 17:14:03 +0100

    Custom fields in ApplicationUser model.

---
+++ src/KPcore/Data/Migrations/20161127160920_CustomFieldsInAppUserModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KPcore.Data.Migrations
+{
+    public partial class CustomFieldsInAppUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "IndexNumber",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IndexNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "AspNetUsers");
+        }
+    }
+}
--- src/KPcore/Models/ApplicationUser.cs
+++ src/KPcore/Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -9,5 +10,17 @@ namespace KPcore.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [StringLength(30)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(30)]
+        [Required]
+        public string LastName { get; set; }
+
+        public int? IndexNumber { get; set; }
+
+        [Required]
+        public int Status { get; set; }
     }
 }
