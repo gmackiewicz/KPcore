7977df7d2b9c5e37e1ed2ea14cd8011ad8240417
Gabriel <gabriel.mackiewicz@gmail.com>
2016-11-29 21:16:59 +0100

    Move AccountViewModels from Model to ViewModels namespace.

--- src/KPcore/ViewModels/AccountViewModels/VerifyCodeViewModel.cs
+++ src/KPcore/ViewModels/AccountViewModels/VerifyCodeViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace KPcore.Models.AccountViewModels
+namespace KPcore.ViewModels.AccountViewModels
 {
     public class VerifyCodeViewModel
     {
