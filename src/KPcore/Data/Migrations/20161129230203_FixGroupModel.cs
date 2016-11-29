a4f2a1f3b5b9eac2bbdbcf37862afd521bf52901
Gabriel <gabriel.mackiewicz@gmail.com>
2016-11-30 00:04:10 +0100

    Fix Group model - it now has reference to Topic instead of Subject.

---
+++ src/KPcore/Data/Migrations/20161129230203_FixGroupModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KPcore.Data.Migrations
+{
+    public partial class FixGroupModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Subjects_SubjectId",
+                table: "Groups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Groups_SubjectId",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectId",
+                table: "Groups");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TopicId",
+                table: "Groups",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_TopicId",
+                table: "Groups",
+                column: "TopicId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Topics_TopicId",
+                table: "Groups",
+                column: "TopicId",
+                principalTable: "Topics",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Topics_TopicId",
+                table: "Groups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Groups_TopicId",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "TopicId",
+                table: "Groups");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SubjectId",
+                table: "Groups",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_SubjectId",
+                table: "Groups",
+                column: "SubjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Subjects_SubjectId",
+                table: "Groups",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
