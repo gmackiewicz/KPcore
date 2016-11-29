3b9e0273749708941745c2ae417eec609c3ef817
Gabriel <gabriel.mackiewicz@gmail.com>
2016-11-29 22:54:30 +0100

    Implement admin panel with adding new subject feature. #7

---
+++ src/KPcore/Controllers/AdminController.cs
@@ -0,0 +1,104 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using KPcore.Data;
+using KPcore.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using KPcore.Models;
+using KPcore.Services;
+using KPcore.ViewModels.AdminViewModels;
+
+namespace KPcore.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private readonly ISubjectRepository _subjectRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminController(
+            UserManager<ApplicationUser> userManager,
+            ISubjectRepository subjectRepository)
+        {
+            _userManager = userManager;
+            _subjectRepository = subjectRepository;
+        }
+
+        // GET: /Admin/Index
+        [HttpGet]
+        public async Task<IActionResult> Index(AdminMessageId? message = null)
+        {
+            ViewData["StatusMessage"] =
+                message == AdminMessageId.AddSubjectSuccess ? "A new subject has been added."
+                : message == AdminMessageId.Error ? "An error has occurred."
+                : "";
+
+            var user = await GetCurrentUserAsync();
+            if (user == null || user.Status != 2)
+            {
+                return View("Error");
+            }
+            var model = new AdminIndexViewModel
+            {
+                Subjects = _subjectRepository.ListAll()
+            };
+            return View(model);
+        }
+        
+        // GET: /Admin/AddSubject
+        public IActionResult AddSubject()
+        {
+            return View();
+        }
+
+        // POST: /Admin/AddSubject
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddSubject(AddSubjectViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            // Generate the token and send it
+            var user = await GetCurrentUserAsync();
+            if (user == null || user.Status != 2)
+            {
+                ModelState.AddModelError(string.Empty, "Failed to add new subject");
+                return View(model);
+            }
+            var subject = new Subject
+            {
+                Name = model.Name,
+                Description = model.Description
+            };
+            _subjectRepository.Add(subject);
+            return RedirectToAction(nameof(Index), new { Message = AdminMessageId.AddSubjectSuccess });
+        }
+        
+        #region Helpers
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        public enum AdminMessageId
+        {
+            AddSubjectSuccess,
+            Error,
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return _userManager.GetUserAsync(HttpContext.User);
+        }
+
+        #endregion
+    }
+}
