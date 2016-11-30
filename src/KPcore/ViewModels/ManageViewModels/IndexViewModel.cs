97ad7c9900996e4250815e42e4afcc1ad60fa90e
Aicero <gabriel.mackiewicz@gmail.com>
2016-11-30 13:27:18 +0100

    Remove unused views.

--- src/KPcore/ViewModels/ManageViewModels/IndexViewModel.cs
+++ src/KPcore/ViewModels/ManageViewModels/IndexViewModel.cs
@@ -8,13 +8,7 @@ namespace KPcore.ViewModels.ManageViewModels
         public int UserStatus { get; set; }
 
         public bool HasPassword { get; set; }
-
-        public IList<UserLoginInfo> Logins { get; set; }
-
-        public string PhoneNumber { get; set; }
-
-        public bool TwoFactor { get; set; }
-
+        
         public bool BrowserRemembered { get; set; }
     }
 }
