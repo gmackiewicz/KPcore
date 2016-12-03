c53de4fb4c22520025964af6ad0fc6150b6c9ab1
Tomasz Osipiak <osi.tomasz@gmail.com>
2016-12-04 00:00:31 +0100

    Simple validation for group name change added

--- src/KPcore/wwwroot/js/site.js
+++ src/KPcore/wwwroot/js/site.js
@@ -1,6 +1,18 @@
 ﻿// Write your Javascript code.
 $(document).ready(function () {
     $("#btn-edit, #btn-cancel").click(function () {
-            $("#group-name-display, #group-name-edit, #btn-edit").toggleClass("hidden");
+        $("#group-name-display, #group-name-edit, #btn-edit").toggleClass("hidden");
+        $("#group-name-validation-error").addClass("hidden");
+    });
+    $('#group-name-input').on('input', function () {
+        var input = $(this);
+        var isNotEmpty = input.val();
+        if (isNotEmpty) {
+            $("#group-name-validation-error").addClass("hidden");
+            $("#grp-name-submit").removeClass("hidden");
+        } else {
+            $("#grp-name-submit").addClass("hidden");
+            $("#group-name-validation-error").removeClass("hidden");
+        }
     });
 });
