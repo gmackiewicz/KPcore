8be8730bc730a57e298af92971b32d13ce631a13
Gabriel Mackiewicz <gabriel.mackiewicz@gmail.com>
2016-12-04 16:02:34 +0100

    Fix subject deletion.

---
+++ src/KPcore/Data/Migrations/20161204150134_CascadeDeleteTopicsOnSubjectDeletion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KPcore.Data.Migrations
+{
+    public partial class CascadeDeleteTopicsOnSubjectDeletion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Topics_Subjects_SubjectId",
+                table: "Topics");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Topics_Subjects_SubjectId",
+                table: "Topics",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Topics_Subjects_SubjectId",
+                table: "Topics");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Topics_Subjects_SubjectId",
+                table: "Topics",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
