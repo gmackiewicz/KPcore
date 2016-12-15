4a8143f9f880562fb60d81a3278bf5dcfb921dc7
Gabriel Mackiewicz <gabriel.mackiewicz@gmail.com>
2016-12-15 15:01:31 +0100

    Add teacher validation during register process.

--- src/KPcore/ViewModels/ManageViewModels/VerifyTeacherViewModel.cs
+++ src/KPcore/ViewModels/ManageViewModels/VerifyTeacherViewModel.cs
@@ -5,6 +5,7 @@ namespace KPcore.ViewModels.ManageViewModels
     public class VerifyTeacherViewModel : BaseViewModel
     {
         [Required]
+        [DataType(DataType.Password)]
         public string Code { get; set; }
     }
 }
