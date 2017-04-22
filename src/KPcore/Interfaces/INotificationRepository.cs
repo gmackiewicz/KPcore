6e2cb73421608cbb7b5bb2547f400336a6c6f0f8
Gabriel Mackiewicz <gabriel.mackiewicz@gmail.com>
2017-04-22 21:08:04 +0200

    Notyfikacje po dodaniu komentarza w grupie

--- src/KPcore/Interfaces/INotificationRepository.cs
+++ src/KPcore/Interfaces/INotificationRepository.cs
@@ -6,5 +6,6 @@ namespace KPcore.Interfaces
     public interface INotificationRepository
     {
         IEnumerable<Notification> GetUsersNotofications(int userId = 0);
+        void AddNotification(string msg, int groupId);
     }
 }
